d133f1e011400bf99adc2305c0698d46a4b73b5e
IvanNSwart <iswart@jhb.dvt.co.za>
2022-03-15 08:44:02 +0200

    refactor: :construction: Added repos api call
    Adding an additional api call

--- Github-Project/src/Services/githubApi.ts
+++ Github-Project/src/Services/githubApi.ts
@@ -8,3 +8,9 @@ export function search(criteria: string): Promise<Profile> {
 		.then((res) => res.data);
 	return response;
 }
+export function repos(criteria: string): Promise<Profile> {
+	const response = fetch(`https://api.github.com/users/${criteria}/repos`)
+		.then((res) => res.json())
+		.then((res) => res.data);
+	return response;
+}
