43d9676491c465f96a6ea465719bebdf4e4077ea
IvanNSwart <iswart@jhb.dvt.co.za>
2022-03-15 08:38:58 +0200

    style: :art: Page styling and basic structure.
    I styled the whole github profile page and added stronglytyped reducer.

--- Github-Project/src/ReduxStore/profileSlice.ts
+++ Github-Project/src/ReduxStore/profileSlice.ts
@@ -1,51 +1,14 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { Profile, ProfileState } from "./profile"
-import { RootState } from "./store"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Profile, ProfileState } from "./profile";
+import { RootState } from "./store";
 
-const initialState: Profile[] = []
+const initialState: Profile[] = [];
 
 export const profileSlice = createSlice({
 	name: "profiles",
 	initialState,
 	reducers: {
-		addToSearch: (
-			state,
-			action: PayloadAction<{
-				login: string
-				id: number
-				node_id: string
-				avatar_url: string
-				gravatar_id: string
-				url: string
-				html_url: string
-				followers_url: string
-				following_url: string
-				gists_url: string
-				starred_url: string
-				subscriptions_url: string
-				organizations_url: string
-				repos_url: string
-				events_url: string
-				received_events_url: string
-				type: string
-				site_admin: boolean
-				name: string
-				company?: any
-				blog: string
-				location?: any
-				email?: any
-				hireable?: any
-				bio: string
-				twitter_username?: any
-				public_repos: number
-				public_gists: number
-				followers: number
-				following: number
-				created_at: string
-				updated_at: string
-				status: ProfileState
-			}>
-		) => [
+		addToSearch: (state, action: PayloadAction<Profile>) => [
 			...state,
 			{
 				login: action.payload.login,
@@ -59,14 +22,11 @@ export const profileSlice = createSlice({
 				following_url: action.payload.following_url,
 				gists_url: action.payload.gists_url,
 				starred_url: action.payload.starred_url,
-				subscriptions_url:
-					action.payload.subscriptions_url,
-				organizations_url:
-					action.payload.organizations_url,
+				subscriptions_url: action.payload.subscriptions_url,
+				organizations_url: action.payload.organizations_url,
 				repos_url: action.payload.repos_url,
 				events_url: action.payload.events_url,
-				received_events_url:
-					action.payload.received_events_url,
+				received_events_url: action.payload.received_events_url,
 				type: action.payload.type,
 				site_admin: action.payload.site_admin,
 				name: action.payload.name,
@@ -76,8 +36,7 @@ export const profileSlice = createSlice({
 				email: action.payload.email,
 				hireable: action.payload.hireable,
 				bio: action.payload.bio,
-				twitter_username:
-					action.payload.twitter_username,
+				twitter_username: action.payload.twitter_username,
 				public_repos: action.payload.public_repos,
 				public_gists: action.payload.public_gists,
 				followers: action.payload.followers,
@@ -90,18 +49,15 @@ export const profileSlice = createSlice({
 		setSelectedProfile: (
 			state,
 			action: PayloadAction<{
-				login: string
-				status: ProfileState
+				login: string;
+				status: ProfileState;
 			}>
 		) => {
-			const { login, status } = action.payload
+			const { login, status } = action.payload;
 			const updateStatus = (
 				status: ProfileState,
 				newStatus: ProfileState
-			) =>
-				status === newStatus
-					? ProfileState.NOTSELECTED
-					: newStatus
+			) => (status === newStatus ? ProfileState.NOTSELECTED : newStatus);
 
 			return [
 				...state.map((profile) =>
@@ -109,23 +65,20 @@ export const profileSlice = createSlice({
 						? profile
 						: {
 								...profile,
-								status: updateStatus(
-									profile.status,
-									status
-								),
+								status: updateStatus(profile.status, status),
 						  }
 				),
-			]
+			];
 		},
 	},
-})
+});
 
-export const { setSelectedProfile } = profileSlice.actions
+export const { setSelectedProfile } = profileSlice.actions;
 
-export const selectProfile = (state: RootState) => state.profiles
+export const selectProfile = (state: RootState) => state.profiles;
 export const selectProfileByLogin = (state: RootState, login: string) =>
 	state.profiles.find(
 		(profile: { login: string }) => profile.login === login
-	)
+	);
 
-export default profileSlice.reducer
+export default profileSlice.reducer;
--- Github-Project/src/Services/githubApi.ts
+++ Github-Project/src/Services/githubApi.ts
@@ -8,18 +8,3 @@ export function search(criteria: string): Promise<Profile> {
 		.then((res) => res.data);
 	return response;
 }
-// function searchTest( criteria: string) {
-//     const { isLoading, error, data } = useQuery('repoData', () =>
-//       fetch(`https://api.github.com/users/${criteria}`).then(res =>
-//         res.json()
-//       )
-//     )
-
-//     if (isLoading) return 'Loading...'
-
-//     if (error) return 'An error has occurred: ' + error.message
-
-//     return (
-
-//     )
-//   }
